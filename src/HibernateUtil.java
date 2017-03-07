a4e59ef69ce71f3a4921f3ad6261ef864ee47a87
andrew.simakov <andrake16@gmail.com>
2017-03-07 16:11:10 +0700

    first

---
+++ src/HibernateUtil.java
@@ -0,0 +1,58 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.sql.ordering.antlr.Factory;
+
+/**
+ * Created by ND on 3/7/2017.
+ */
+public class HibernateUtil {
+    public static SessionFactory factory;
+
+    public static void main(String[] args) {
+
+        init();
+
+        Video video = new Video();
+        video.setId(123L);
+        video.setDescription("the Movie");
+
+
+        Actor actor = new Actor();
+        actor.setName("John Wayne");
+        Cast cast = new Cast();
+        cast.setVideo(video);
+        cast.setActor(actor);
+
+
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.saveOrUpdate(video);
+        session.saveOrUpdate(actor);
+        session.saveOrUpdate(cast);
+        session.getTransaction().commit();
+        session.close();
+
+
+        //Session session = factory.openSession();
+        //Actor actor = (Actor)session.get(Actor.class,new Long(1));
+        //session.close();
+        //System.out.println(actor.getName());
+
+
+
+    }
+
+    private static void init() {
+        AnnotationConfiguration aconf = new AnnotationConfiguration()
+                .addAnnotatedClass(Video.class)
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Cast.class);
+        Configuration conf = aconf.configure();
+        factory = conf.buildSessionFactory();
+    }
+
+
+
+}
